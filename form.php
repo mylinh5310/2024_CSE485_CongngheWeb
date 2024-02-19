6c13a2e84b21c5dbe7617b430d1ae1db2aaf59c2
mylinh5310 <thptdaian.lethimailinhk1821@gmail.com>
2024-02-19 11:52:24 +0700

    push code

---
+++ form.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="O:/ki2_nam3/Laptrinhweb/ckeditor5-build-classic-41.1.0/ckeditor5-build-classic/ckeditor.js.map"></script>
+    <title>Document</title>
+</head>
+<body>
+<?php
+$countries=array(
+    'NAM ĐỊNH','BẮC NINH','VĨNH PHÚC','NINH BÌNH','THÁI BÌNH','HÀ NỘI','TP HỒ CHÍ MINH',
+    'VĨNH LONG','TIỀN GIANG','KIÊN GIANG','HẬU GIANG','TRA VINH','BINH PHUOC','HA TĨNH','THANH HÓA','HÀ GIANG'
+);
+?>
+</body>
+</html>
